51e5794e3b9b16f002af387c09d276bc1177942d
Carlos Gil <carlos.gil@estudiantes.utec.edu.uy>
2026-04-11 16:50:49 -0300

    Limpieza

--- src/main/java/com/chat/config/AppConfig.java
+++
@@ -1,8 +0,0 @@
-package com.chat.config;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class AppConfig extends Application {
-}
