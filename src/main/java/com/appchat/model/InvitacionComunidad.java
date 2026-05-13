0e3dbfb33a4f38673eb07ff001b0c31f7518b486
aldanarodrigu <aldanarodriguez5407@gmail.com>
2026-05-13 15:34:55 -0300

    fix

--- src/main/java/com/appchat/model/InvitacionComunidad.java
+++ src/main/java/com/appchat/model/InvitacionComunidad.java
@@ -5,13 +5,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "invitacion_comunidad")
 public class InvitacionComunidad {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private Long comunidadId;
